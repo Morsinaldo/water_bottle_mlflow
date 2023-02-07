8d7e764ecb503bef6b95329273dc8ee60df25ab3
Morsinaldo <morsinaldo.js@outlook.com>
2023-02-07 08:23:27 -0300

    feat: add data_segregation files

--- data_segregation/run.py
+++ data_segregation/run.py
@@ -11,14 +11,9 @@ be saved as artifacts and uploaded to W&B.
 # import the necessary packages
 import argparse
 import logging
-import os
 import joblib
-import pandas as pd
 import wandb
 from sklearn.model_selection import train_test_split
-
-# login to wandb
-wandb.login()
 
 # configure logging
 logging.basicConfig(level=logging.INFO,
@@ -32,7 +27,6 @@ def process_args(args):
     """
     Arguments:
       args - command line arguments
-      args.project_name - name of the project
       args.artifact_name_feature - name of the artifact containing the clean features
       args.artifact_name_target - name of the artifact containing the labels
       args.train_feature_artifact - name of the artifact containing the training features
@@ -45,8 +39,12 @@ def process_args(args):
       args.val_size - percentage of the data to be used for validation
       args.seed - seed for random number generator
     """
+
+    # login to wandb
+    wandb.login()
+
     # open the W&B project created in the Fetch step
-    run = wandb.init(entity="morsinaldo",project=args.project_name, job_type="data_segregation")
+    run = wandb.init(job_type="data_segregation")
 
     logger.info("Downloading and reading clean data artifact")
     clean_data = run.use_artifact(args.artifact_name_feature)
@@ -62,11 +60,11 @@ def process_args(args):
 
     # partition the data into training, test splits using 75% of
     # the data for training and the remaining 25% for test
-    (train_x, test_x, train_y, test_y) = train_test_split(data, label,test_size=args.test_size, random_state=args.seed)
+    (train_x, test_x, train_y, test_y) = train_test_split(data, label, test_size=args.test_size, random_state=args.seed)
 
     # partition the training into training, validation splits using 75% of
     # the training set for training and the remaining 25% for validation
-    (train_x, val_x, train_y, val_y) = train_test_split(train_x, train_y,test_size=args.val_size, random_state=args.seed)
+    (train_x, val_x, train_y, val_y) = train_test_split(train_x, train_y, test_size=args.val_size, random_state=args.seed)
 
     logger.info("Train x: {}".format(train_x.shape))
     logger.info("Train y: {}".format(train_y.shape))
@@ -150,33 +148,31 @@ def process_args(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Data Segregation")
 
-    parser.add_argument("--project_name", type=str, default="water_bottle_classifier", required=False)
-    parser.add_argument("--artifact_name_feature", type=str, default="clean_features:latest", required=False)
-    parser.add_argument("--artifact_name_target", type=str, default="labels:latest", required=False)
-    parser.add_argument("--train_feature_artifact", type=str, default="train_x", required=False)
-    parser.add_argument("--train_target_artifact", type=str, default="train_y", required=False)
-    parser.add_argument("--val_feature_artifact", type=str, default="val_x", required=False)
-    parser.add_argument("--val_target_artifact", type=str, default="val_y", required=False)
-    parser.add_argument("--test_feature_artifact", type=str, default="test_x", required=False)
-    parser.add_argument("--test_target_artifact", type=str, default="test_y", required=False)
-    parser.add_argument("--test_size", type=float, default=0.25, required=False)
-    parser.add_argument("--val_size", type=float, default=0.25, required=False)
-    parser.add_argument("--seed", type=int, default=42, required=False)
+    parser.add_argument("--artifact_name_feature", type=str,required=True)
+    parser.add_argument("--artifact_name_target", type=str, required=True)
+    parser.add_argument("--train_feature_artifact", type=str, required=True)
+    parser.add_argument("--train_target_artifact", type=str, required=True)
+    parser.add_argument("--val_feature_artifact", type=str, required=True)
+    parser.add_argument("--val_target_artifact", type=str, required=True)
+    parser.add_argument("--test_feature_artifact", type=str, required=True)
+    parser.add_argument("--test_target_artifact", type=str, required=True)
+    parser.add_argument("--test_size", type=float, required=True)
+    parser.add_argument("--val_size", type=float, required=True)
+    parser.add_argument("--seed", type=int, required=True)
 
     ARGS = parser.parse_args()
 
     process_args(ARGS)
 
 # Run the script
-# mlflow run . -P project_name="water_bottle_classifier" \
-#               -P artifact_name_feature="clean_features:latest" \
-#               -P artifact_name_target="labels:latest" \
-#               -P train_feature_artifact="train_x" \
-#               -P train_target_artifact="train_y" \
-#               -P val_feature_artifact="val_x" \
-#               -P val_target_artifact="val_y" \
-#               -P test_feature_artifact="test_x" \
-#               -P test_target_artifact="test_y" \
-#               -P test_size=0.25 \
-#               -P val_size=0.25 \
-#               -P seed=42
+    # mlflow run . -P artifact_name_feature="clean_features:latest" \
+    #               -P artifact_name_target="labels:latest" \
+    #               -P train_feature_artifact="train_x" \
+    #               -P train_target_artifact="train_y" \
+    #               -P val_feature_artifact="val_x" \
+    #               -P val_target_artifact="val_y" \
+    #               -P test_feature_artifact="test_x" \
+    #               -P test_target_artifact="test_y" \
+    #               -P test_size=0.25 \
+    #               -P val_size=0.25 \
+    #               -P seed=42
