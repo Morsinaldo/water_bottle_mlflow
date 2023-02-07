f801bc13e76abc17caba6e3bf2c3d53e59fde7de
Morsinaldo <morsinaldo.js@outlook.com>
2023-02-07 08:27:45 -0300

    feat: add train files

--- train/helpers/model.py
+++ train/helpers/model.py
@@ -1,97 +1,11 @@
-import cv2
-import tensorflow as tf
+"""
+Creator: Morsinaldo Medeiros
+Date: 06-02-2023
+Description: This script contains the model class.
+"""
 import numpy as np
-import os
-from tensorflow.keras.preprocessing.image import img_to_array
-
 import torch
-from torch.utils.data import Dataset
-from torchvision import transforms, models
-
-class SimplePreprocessor:
-    """ Simple Preprocessor class to resize the image to a fixed size ignoring the aspect ratio. """
-    def __init__(self, width, height, inter=cv2.INTER_AREA):
-		# store the target image width, height, and interpolation
-		# method used when resizing
-        self.width = width
-        self.height = height
-        self.inter = inter
-        
-    def preprocess(self, image):
-		# resize the image to a fixed size, ignoring the aspect
-		# ratio
-        return cv2.resize(image, (self.width, self.height),interpolation=self.inter)
-
-class ImageToArrayPreprocessor:
-    """ ImageToArrayPreprocessor class to convert the image to a numpy array. """
-    def __init__(self, dataFormat=None):
-		# store the image data format
-        self.dataFormat = dataFormat
-        
-    def preprocess(self, image):
-		# apply the Keras utility function that correctly rearranges
-		# the dimensions of the image
-        return img_to_array(image, data_format=self.dataFormat)
-
-class SimpleDatasetLoader:
-    """ SimpleDatasetLoader class to load the image dataset from disk and apply the preprocessors. """
-    def __init__(self, preprocessors=None, logger=None):
-		# store the image preprocessor
-        self.preprocessors = preprocessors
-        self.logger = logger
-
-		# if the preprocessors are None, initialize them as an
-		# empty list
-        if self.preprocessors is None:
-            self.preprocessors = []
-
-    def load(self, imagePaths, verbose=-1):
-            # initialize the list of features and labels
-        data = []
-        labels = []
-
-            # loop over the input images
-        for (i, imagePath) in enumerate(imagePaths):
-            # load the image and extract the class label assuming
-            # that our path has the following format:
-            # /path/to/dataset/{class}/{image}.jpg
-            # e.g "img example: ./artifacts/animals_raw_data:v0/dogs/dogs_00892.jpg"
-            # imagePath.split(os.path.sep)[-2] will return "dogs"
-            image = cv2.imread(imagePath)
-            label = imagePath.split(os.path.sep)[-2]
-
-            # check to see if our preprocessors are not None
-            if self.preprocessors is not None:
-                # loop over the preprocessors and apply each to
-                # the image
-                for p in self.preprocessors:
-                    image = p.preprocess(image)
-            
-            # treat our processed image as a "feature vector"
-            # by updating the data list followed by the labels
-            data.append(image)
-            labels.append(label)
-    
-            # show an update every `verbose` images
-            if verbose > 0 and i > 0 and (i + 1) % verbose == 0:
-                self.logger.info("[INFO] processed {}/{}".format(i + 1,len(imagePaths)))
-
-            # return a tuple of the data and labels
-        return (np.array(data), np.array(labels))
-
-class ImageDataset(Dataset):
-    """Custom Dataset for loading images"""
-    def __init__(self, X, y):
-        self.x = X
-        self.y = y
-        self.n_samples = len(X)
-        
-    def __getitem__(self, index):
-        transform = transforms.Compose([transforms.ToTensor()])
-        return transform(self.x[index]), torch.tensor(self.y[index], dtype=torch.int64)
-    
-    def __len__(self):
-        return self.n_samples
+from torchvision import models
 
 class MyVisionTransformerModel():
     """ MyVisionTransformerModel class to load the model and predict the image. """
@@ -113,8 +27,13 @@ class MyVisionTransformerModel():
             self.logger.info("Using CPU")
             self.device = torch.device('cpu')
         self.model.to(self.device)
-        self.logger.info(f"Using {self.device} device")
 
+    def parameters(self):
+        return self.model.parameters()
+
+    def state_dict(self):
+        return self.model.state_dict()
+    
     def load_model(self, model_path):
         self.logger.info(f"Loading model from {model_path}")
         self.model.load_state_dict(torch.load(model_path, map_location=self.device))
--- train/run.py
+++ train/run.py
@@ -1,35 +1,27 @@
-import os
+"""
+Creator: Morsinaldo Medeiros
+Date: 06-02-2023
+Description: This script is responsible for running the train step of the pipeline.
+"""
+
 import wandb
 import joblib
 import logging
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.preprocessing import LabelEncoder
 from sklearn.utils.class_weight import compute_class_weight
 
 import torch
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, models
+from torch.utils.data import DataLoader
 from torch import nn
 
-from utils.helpers import ImageDataset, MyVisionTransformerModel
-
-# batch_size = 50
-
-# class ImageDataset(Dataset):
-#     def __init__(self, X, y):
-#         self.x = X
-#         self.y = y
-#         self.n_samples = len(X)
-        
-#     def __getitem__(self, index):
-#         transform = transforms.Compose([transforms.ToTensor()])
-#         return transform(self.x[index]), torch.tensor(self.y[index], dtype=torch.int64)
-    
-#     def __len__(self):
-#         return self.n_samples
+from helpers.images_processing import ImageDataset
+from helpers.model import MyVisionTransformerModel
 
 # configure logging
 logging.basicConfig(level=logging.INFO,
@@ -39,203 +31,184 @@ logging.basicConfig(level=logging.INFO,
 # reference for a logging obj
 logger = logging.getLogger()
 
-classes = {'Full  Water level': 0, 'Half water level': 1, 'Overflowing': 2}
+# classes = {'Full  Water level': 0, 'Half water level': 1, 'Overflowing': 2}
 
-# since we are using Jupyter Notebooks we can replace our argument
-# parsing code with *hard coded* arguments and values
-args = {
-  "project_name": "water_bottle_classifier",
-  "train_feature_artifact": "train_x:latest",
-  "train_target_artifact": "train_y:latest",
-  "val_feature_artifact": "val_x:latest",
-  "val_target_artifact": "val_y:latest",
-  "encoder": "target_encoder",
-  "inference_model": "vit_l_32.pth"
-}
+def process_args(args):
+    """
+    Arguments:
+        args - command line arguments
+        args.train_feature_artifact - name of the artifact containing the train features
+        args.train_target_artifact - name of the artifact containing the train target
+        args.val_feature_artifact - name of the artifact containing the validation features
+        args.val_target_artifact - name of the artifact containing the validation target
+        args.encoder - name of the artifact containing the encoder
+        args.inference_model - name of the artifact containing the inference model
+        args.batch_size - batch size for the dataloader
+        args.seed - seed for the random number generator
+        args.epochs - number of epochs to train the model
+        args.learning_rate - learning rate for the optimizer
+    """
 
-HYP = {
-        'seed': 44,
-        'batch_size': 50,
-        'img_size': (225,225),
-        'epochs': 50,
-        'patience': 5,
-        'learning_rate': 0.0001,
-    }
+    wandb.login()
 
-# open the W&B project created in the Fetch step
-run = wandb.init(entity="morsinaldo",project=args["project_name"], job_type="Train")
+    # open the W&B project created in the Fetch step
+    run = wandb.init(job_type="Train")
 
-logger.info("Downloading the train and validation data")
-# train x
-train_x_artifact = run.use_artifact(args["train_feature_artifact"])
-train_x_path = train_x_artifact.file()
+    logger.info("Downloading the train and validation data")
+    # train x
+    train_x_artifact = run.use_artifact(args.train_feature_artifact)
+    train_x_path = train_x_artifact.file()
 
-# train y
-train_y_artifact = run.use_artifact(args["train_target_artifact"])
-train_y_path = train_y_artifact.file()
+    # train y
+    train_y_artifact = run.use_artifact(args.train_target_artifact)
+    train_y_path = train_y_artifact.file()
 
-# validation x
-val_x_artifact = run.use_artifact(args["val_feature_artifact"])
-val_x_path = val_x_artifact.file()
+    # validation x
+    val_x_artifact = run.use_artifact(args.val_feature_artifact)
+    val_x_path = val_x_artifact.file()
 
-# validation y
-val_y_artifact = run.use_artifact(args["val_target_artifact"])
-val_y_path = val_y_artifact.file()
+    # validation y
+    val_y_artifact = run.use_artifact(args.val_target_artifact)
+    val_y_path = val_y_artifact.file()
 
-# unpacking the artifacts
-train_x = joblib.load(train_x_path)
-train_y = joblib.load(train_y_path)
-val_x = joblib.load(val_x_path)
-val_y = joblib.load(val_y_path)
+    # unpacking the artifacts
+    train_x = joblib.load(train_x_path)
+    train_y = joblib.load(train_y_path)
+    val_x = joblib.load(val_x_path)
+    val_y = joblib.load(val_y_path)
 
-# log the shape of the data
-logger.info("Train x: {}".format(train_x.shape))
-logger.info("Train y: {}".format(train_y.shape))
-logger.info("Validation x: {}".format(val_x.shape))
-logger.info("Validation y: {}".format(val_y.shape))
+    # log the shape of the data
+    logger.info("Train x: {}".format(train_x.shape))
+    logger.info("Train y: {}".format(train_y.shape))
+    logger.info("Validation x: {}".format(val_x.shape))
+    logger.info("Validation y: {}".format(val_y.shape))
 
-# map train_y and val_y to 0, 1, 2
-train_y = np.array([classes[i] for i in train_y])
-val_y = np.array([classes[i] for i in val_y])
+    # encode the target
+    logger.info("Encoding the target")
+    encoder = LabelEncoder()
 
-# create the dataset and dataloader
-train_dataset = ImageDataset(train_x, train_y)
-train_loader = DataLoader(train_dataset, batch_size=HYP['batch_size'], shuffle=True)
-test_dataset = ImageDataset(val_x, val_y)
-test_loader = DataLoader(test_dataset, batch_size=HYP['batch_size'])
+    # fit the encoder
+    train_y = encoder.fit_transform(train_y)
+    val_y = encoder.transform(val_y)
 
-# compute class weights
-class_weights = compute_class_weight(class_weight='balanced', classes=np.unique(train_y), y=train_y)
-logger.info("Class weights: {}".format(class_weights))
+    # save the encoder
+    logger.info("Saving the encoder")
+    joblib.dump(encoder, args.encoder)
 
-# total steps
-total_steps = len(train_loader)
-logger.info("Total steps: {}".format(total_steps))
+    # create the dataset and dataloader
+    train_dataset = ImageDataset(train_x, train_y)
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+    test_dataset = ImageDataset(val_x, val_y)
+    test_loader = DataLoader(test_dataset, batch_size=args.batch_size)
 
-# define the model
-# model = models.vit_l_32(pretrained=True)
-# for param in model.parameters():
-#     param.requires_grad = False
+    # compute class weights
+    class_weights = compute_class_weight(class_weight='balanced', classes=np.unique(train_y), y=train_y)
+    logger.info("Class weights: {}".format(class_weights))
 
-water_level_encoding = {}
-for index, label in enumerate(train_y):
-    water_level_encoding[label] = index
-water_level_encoding
+    # total steps
+    total_steps = len(train_loader)
+    logger.info("Total steps: {}".format(total_steps))
 
-# model.heads.head = nn.Linear(1024, len(water_level_encoding))
+    model = MyVisionTransformerModel(len_encoding=len(encoder.classes_), logger=logger, pretrained=True)
 
-# device = torch.device('cuda' if torch.cuda.is_available else 'cpu')
-# logger.info("Device: {}".format(device))
-# model.to(device)
+    criterion = nn.CrossEntropyLoss(weight=torch.from_numpy(class_weights.astype(np.float32)).to(model.device))
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
 
-model = MyVisionTransformerModel(num_classes=len(water_level_encoding))
+    history = model.train(train_loader, test_loader, criterion, optimizer, total_steps, args.epochs)
 
-criterion = nn.CrossEntropyLoss(weight=torch.from_numpy(class_weights.astype(np.float32)).to(model.device))
-optimizer = torch.optim.Adam(model.parameters(), lr=HYP['learning_rate'])
+    test_predictions, test_labels = model.predict(test_loader)
 
-# history = {
-#     'train_loss': [],
-#     'train_acc': [],
-#     'val_loss': [],
-#     'val_acc': []
+    fig_confusion_matrix, ax = plt.subplots(1,1,figsize=(7,4))
+    ConfusionMatrixDisplay(confusion_matrix(test_predictions,
+                                            test_labels),
+                          display_labels=encoder.classes_).plot(values_format=".0f",ax=ax)
+
+    ax.set_xlabel("True Label")
+    ax.set_ylabel("Predicted Label")
+    ax.grid(False)
+
+    # plot the training and validation loss
+    fig_loss, ax = plt.subplots(1,1,figsize=(7,4))
+    ax.plot(history['train_loss'], label='train')
+    ax.plot(history['val_loss'], label='validation')
+    ax.set_xlabel("Epoch")
+    ax.set_ylabel("Loss")
+    ax.legend()
+
+    fig_confusion_matrix.savefig("confusion_matrix.png")
+    fig_loss.savefig("loss.png")
+
+    # save the model
+    torch.save(model.state_dict(), args.inference_model)
+
+    # send the model to the wandb as an artifact
+    logger.info("Saving the model")
+    model_artifact = wandb.Artifact(args.inference_model, type='model')
+    model_artifact.add_file(args.inference_model)
+    wandb.log_artifact(model_artifact)
+
+    # send the encoder to wandb as an artifact
+    logger.info("Saving the encoder")
+    encoder_artifact = wandb.Artifact(args.encoder, type='encoder')
+    encoder_artifact.add_file(args.encoder)
+    wandb.log_artifact(encoder_artifact)
+
+    # Uploading figures
+    logger.info("Uploading figures")
+    run.log(
+        {
+            "confusion_matrix": wandb.Image(fig_confusion_matrix),
+            "loss": wandb.Image(fig_loss),
+
+            # "other_figure": wandb.Image(other_fig)
+        }
+    )
+
+    # # send the confusion matrix and loss plot to wandb
+    # logger.info("Saving the confusion matrix and loss plot")
+    # wandb.log_artifact(wandb.Image(fig_confusion_matrix, caption="Confusion Matrix"))
+    # wandb.log_artifact(wandb.Image(fig_loss, caption="Loss"))
+
+    # # send the training and validation loss figures to wandb
+    # wandb.log({"train_loss": wandb.Image(fig_loss, caption="Train Loss")})
+    # wandb.log({"val_loss": wandb.Image(fig_loss, caption="Validation Loss")})
+
+    run.finish()
+
+# args = {
+#   "project_name": "water_bottle_classifier",
+#   "train_feature_artifact": "train_x:latest",
+#   "train_target_artifact": "train_y:latest",
+#   "val_feature_artifact": "val_x:latest",
+#   "val_target_artifact": "val_y:latest",
+#   "encoder": "target_encoder",
+#   "inference_model": "vit_l_32.pth",
+#   'seed': 44,
+#   'batch_size': 50,
+#   'epochs': 50,
+#   'learning_rate': 0.0001,
 # }
 
-# epochs = HYP['epochs']
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('--train_feature_artifact', type=str, required=True,
+                        help="Name of the artifact containing the training features")
+    parser.add_argument('--train_target_artifact', type=str, required=True,
+                        help="Name of the artifact containing the training target")
+    parser.add_argument('--val_feature_artifact', type=str, required=True,
+                        help="Name of the artifact containing the validation features")
+    parser.add_argument('--val_target_artifact', type=str, required=True,
+                        help="Name of the artifact containing the validation target")
+    parser.add_argument('--encoder', type=str, required=True,
+                        help="Name of the artifact containing the target encoder")
+    parser.add_argument('--inference_model', type=str, required=True,
+                        help="Name of the artifact containing the inference model")
+    parser.add_argument('--batch_size', type=int, required=True, help="Batch size")
+    parser.add_argument('--seed', type=int, required=True, help="Random seed")
+    parser.add_argument('--epochs', type=int, required=True, help="Number of epochs")
+    parser.add_argument('--learning_rate', type=float, required=True, help="Learning rate")
 
-# for epoch in range(HYP['epochs']):
-#     for i, (train_images, train_labels) in enumerate(train_loader):
-#         train_images = train_images.to(device)
-#         train_labels = train_labels.to(device)
-#         outputs = model(train_images.float())
-#         loss = criterion(outputs, train_labels)
-#         optimizer.zero_grad()
-#         loss.backward()
-#         optimizer.step()
-        
-#         with torch.no_grad():
-#             train_images.to('cpu')
-#             train_labels.to('cpu')
-#             predicted_outputs = model(train_images.float())
-#             _, train_predictions = torch.max(predicted_outputs, 1)
-#             n_correct = (train_predictions == train_labels).sum().item()
-#             acc = n_correct/len(train_images)
-            
-#         if (i+1) % 4 == 0:
-#             with torch.no_grad():
-#                 n_test_correct = 0
-#                 n_test = 0
-#                 for test_images, test_labels in test_loader:
-#                     test_images = test_images.to(device)
-#                     test_labels = test_labels.to(device)
-#                     test_outputs = model(test_images.float())
-#                     _, test_predictions = torch.max(test_outputs, 1)
-#                     test_images.to('cpu')
-#                     test_labels.to('cpu')
-#                     n_test_correct += (test_predictions == test_labels).sum().item()
-#                     n_test += len(test_images)
-#                 val_acc = n_test_correct/n_test
-#             logger.info(f'epoch {epoch+1} / {epochs}, step {i+1} / {total_steps}, loss = {loss.item():.4f}, accuracy = {acc:.4f}, val_accuracy = {val_acc:.4f}')
-#             history['train_loss'].append(loss.item())
-#             history['train_acc'].append(acc)
-#             history['val_loss'].append(loss.item())
-#             history['val_acc'].append(val_acc)
+    ARGS = parser.parse_args()
 
-model.train(train_loader, test_loader, criterion, optimizer, HYP['epochs'])
-
-# test_predictions = []
-# test_labels = []
-# with torch.no_grad():
-#     for i, (t_images, t_labels) in enumerate(test_loader):
-#         t_images = t_images.to(device)
-#         t_labels = t_labels.to(device)
-#         outputs = model(t_images.float())
-#         _, predictions = torch.max(outputs, 1)
-#         predictions = predictions.to('cpu')
-#         t_labels = t_labels.to('cpu')
-#         test_predictions.append(predictions.numpy())
-#         test_labels.append(t_labels.numpy())
-
-# test_predictions = np.hstack(test_predictions)
-# test_labels = np.hstack(test_labels)
-
-test_predictions, test_labels = model.predict(test_loader)
-
-fig_confusion_matrix, ax = plt.subplots(1,1,figsize=(7,4))
-ConfusionMatrixDisplay(confusion_matrix(test_predictions,
-                                        test_labels),
-                       display_labels=water_level_encoding).plot(values_format=".0f",ax=ax)
-
-ax.set_xlabel("True Label")
-ax.set_ylabel("Predicted Label")
-ax.grid(False)
-# plt.show()
-
-# plot the training and validation loss
-fig_loss, ax = plt.subplots(1,1,figsize=(7,4))
-ax.plot(history['train_loss'], label='train')
-ax.plot(history['val_loss'], label='validation')
-ax.set_xlabel("Epoch")
-ax.set_ylabel("Loss")
-ax.legend()
-# plt.show()
-
-fig_confusion_matrix.savefig("confusion_matrix.png")
-fig_loss.savefig("loss.png")
-
-# save the model
-torch.save(model.state_dict(), args['inference_model'])
-
-# send the model to the wandb as an artifact
-model_artifact = wandb.Artifact(args['inference_model'], type='model')
-model_artifact.add_file(args['inference_model'])
-wandb.log_artifact(model_artifact)
-
-# send the confusion matrix and loss plot to wandb
-wandb.log_artifact(wandb.Image(fig_confusion_matrix, caption="Confusion Matrix"))
-wandb.log_artifact(wandb.Image(fig_loss, caption="Loss"))
-
-# send the training and validation loss figures to wandb
-wandb.log({"train_loss": wandb.Image(fig_loss, caption="Train Loss")})
-wandb.log({"val_loss": wandb.Image(fig_loss, caption="Validation Loss")})
-
-run.finish()
+    process_args(ARGS)
